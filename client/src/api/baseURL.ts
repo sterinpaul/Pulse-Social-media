606c60d5aeb5dd47350731f9ada110e058d4dcaf
swagbag007 <raju@swagbag.com>
2023-09-30 22:58:35 +0530

    Domain name changed

--- client/src/api/baseURL.ts
+++ client/src/api/baseURL.ts
@@ -1,5 +1,5 @@
-export const BASEURL = "http://localhost:5000/api/"
-export const socketURL = "http://localhost:5000/"
+export const BASEURL = "http://pulz.site/api/"
+export const socketURL = "http://pulz.site/"
 import PROFILE_AVATAR from '../../src/avatar-profile-icon.jpg'
 export const PROFILE_PHOTO = PROFILE_AVATAR
 export const CLOUDINARY_PROFILE_PHOTO_URL = 'https://res.cloudinary.com/pulse-socialmedia/image/upload/v1691507231/profilePics/image-'
