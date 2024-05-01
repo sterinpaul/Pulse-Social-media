624b6b3daddfbfc56e356ba3d3c1520f7fda9544
swagbag007 <raju@swagbag.com>
2024-05-01 21:11:55 +0530

    Test for getting location of client

--- client/src/api/baseURL.ts
+++ client/src/api/baseURL.ts
@@ -1,7 +1,7 @@
-export const BASEURL = "https://pulz.site/api/"
-export const SOCKET_URL = "wss://pulz.site/"
-// export const BASEURL = "http://localhost:5000/api/"
-// export const SOCKET_URL = "ws://localhost:5000/"
+// export const BASEURL = "https://pulz.site/api/"
+// export const SOCKET_URL = "wss://pulz.site/"
+export const BASEURL = "http://localhost:5000/api/"
+export const SOCKET_URL = "ws://localhost:5000/"
 import PROFILE_AVATAR from '../../src/avatar-profile-icon.jpg'
 export const PROFILE_PHOTO = PROFILE_AVATAR
 export const CLOUDINARY_PROFILE_PHOTO_URL = 'https://res.cloudinary.com/pulse-socialmedia/image/upload/v1691507231/profilePics/image-'
